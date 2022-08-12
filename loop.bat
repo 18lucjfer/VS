9730e772e7eae23988ba3874372a0898c63579a5
18lucjfer <111067011+18lucjfer@users.noreply.github.com>
2022-08-12 20:27:06 +0700

    Delete loop.bat

--- loop.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Visual Studio Created!
-tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Can't connecting to Ngrok!" & ping 127.0.0.1 >Nul & exit
-:check
-ping 127.0.0.1 > null
-cls
-echo VS running
-goto check
